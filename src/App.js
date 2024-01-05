610e02fb2a3aa6c90cadf9199f24e0aca6466acd
FelipeZambelloSv <rm99843@fiap.com.br>
2024-01-05 16:16:12 -0300

    header adjustment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import Logo from './componentes/Logo'
 
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
+
         <ul className='opcoes'>
-          <li className='opcao'><p>CATEGORIAS</p></li>
-          <li className='opcao'><p>MINHA ESTANTE</p></li>
-          <li className='opcao'><p>FAVORITOS</p></li>
+          { textoOpcoes.map( (texto) => (
+            <li className='opcao'><p>{texto}</p></li>
+          ) ) }
         </ul>
+        
       </header>
     </div>
   );
