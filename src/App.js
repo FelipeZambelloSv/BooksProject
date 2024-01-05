e3d81233bfc64297efac4bc18ad9b6a228a15c0d
FelipeZambelloSv <rm99843@fiap.com.br>
2024-01-05 20:30:37 -0300

    working with state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './componentes/Header';
+import Pesquisa from './componentes/Pesquisa';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
@@ -7,10 +8,13 @@ const AppContainer = styled.div`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 `
 
+
+
 function App() {
   return (
     <AppContainer>
       <Header/>
+      <Pesquisa />
     </AppContainer>
   );
 }
