eb8c4457df016270b538be67a58d57e9d96f7890
FelipeZambelloSv <rm99843@fiap.com.br>
2024-01-09 18:05:04 -0300

    finished project

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,7 +1,44 @@
+import { livros } from './dadosUltimosLancamentos'
+import { Titulo } from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import styled from 'styled-components'
+import imagemLivro from '../../imagens/livro2.png'
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
 function UltimosLancamentos(){
     return(
-        <h2>Ultimos Lançamentos</h2>
-        
+        <UltimosLancamentosContainer>
+            <Titulo 
+            cor="#EB9B00" 
+            tamanhoFonte="36px">
+            Últimos Lançamentos</Titulo>
+            
+            <NovosLivrosContainer>
+            {livros.map( livro => (
+                <img src={livro.src}/>
+            ))}
+            </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por:"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação Google"
+                img={imagemLivro}
+            />
+        </UltimosLancamentosContainer>
     )
 }
 
